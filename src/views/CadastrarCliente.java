0f9be31e3f938c123859a7cfb84b6c2cdcb99c96
adVentureLion <hugomartinsleao@gmail.com>
2021-07-18 15:26:20 -0300

    add 2 telas

---
+++ src/views/CadastrarCliente.java
@@ -0,0 +1,170 @@
+package views;
+
+import java.awt.BorderLayout;
+import controller.ControleCliente;
+import models.Cliente;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.text.MaskFormatter;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import javax.swing.JTextField;
+import javax.swing.JFormattedTextField;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+import java.awt.event.ActionListener;
+import java.util.Set;
+import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class CadastrarCliente extends JFrame {
+	
+	ControleCliente controle = new ControleCliente();
+	
+
+	private JPanel contentPane;
+	private JTextField tfNome;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
+	
+	private MaskFormatter mask(String mascara){ 
+		MaskFormatter mask = null;
+		try{ 
+			mask = new MaskFormatter(mascara); 
+			}catch(java.text.ParseException ex){
+				
+			} 
+		return mask; 
+		}
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CadastrarCliente frame = new CadastrarCliente();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public CadastrarCliente() {
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosed(WindowEvent e) {
+				controle.atualizarClientesController();
+			}
+		});
+		
+		
+		setResizable(false);
+		setFont(new Font("Century", Font.PLAIN, 12));
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 364, 319);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("NOME");
+		lblNewLabel.setFont(new Font("Century", Font.PLAIN, 16));
+		lblNewLabel.setBounds(10, 20, 68, 25);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblCpf = new JLabel("CPF");
+		lblCpf.setFont(new Font("Century", Font.PLAIN, 16));
+		lblCpf.setBounds(10, 80, 68, 25);
+		contentPane.add(lblCpf);
+		
+		JLabel lblSexo = new JLabel("SEXO");
+		lblSexo.setFont(new Font("Century", Font.PLAIN, 16));
+		lblSexo.setBounds(10, 140, 68, 25);
+		contentPane.add(lblSexo);
+		
+		JLabel lblDataDeNascimento = new JLabel("DATA DE NASCIMENTO");
+		lblDataDeNascimento.setFont(new Font("Century", Font.PLAIN, 16));
+		lblDataDeNascimento.setBounds(10, 200, 203, 25);
+		contentPane.add(lblDataDeNascimento);
+		
+		tfNome = new JTextField();
+		tfNome.setFont(new Font("Century", Font.PLAIN, 12));
+		tfNome.setBounds(140, 17, 198, 34);
+		contentPane.add(tfNome);
+		tfNome.setColumns(10);
+		
+		JFormattedTextField ftfData = new JFormattedTextField(mask("##/##/####"));
+		ftfData.setFont(new Font("Century", Font.PLAIN, 12));
+		ftfData.setBounds(223, 197, 115, 34);
+		contentPane.add(ftfData);
+		
+		JRadioButton rbF = new JRadioButton("FEMININO");
+		buttonGroup.add(rbF);
+		rbF.setFont(new Font("Century", Font.PLAIN, 12));
+		rbF.setBounds(140, 142, 91, 23);
+		contentPane.add(rbF);
+		
+		JRadioButton rbM = new JRadioButton("MASCULINO");
+		buttonGroup.add(rbM);
+		rbM.setFont(new Font("Century", Font.PLAIN, 12));
+		rbM.setBounds(233, 143, 105, 23);
+		contentPane.add(rbM);
+		
+		JFormattedTextField ftfCPF = new JFormattedTextField(mask("###.###.###-##"));
+		ftfCPF.setFont(new Font("Century", Font.PLAIN, 12));
+		ftfCPF.setBounds(140, 77, 198, 34);
+		contentPane.add(ftfCPF);
+		
+		JButton btnNewButton = new JButton("CADASTRAR");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					if(tfNome.getText().equals("") || ftfCPF.getText().equals("   .   .   -  ") || ftfData.getText().equals("  /  /    ") || (!rbF.isSelected() && !rbM.isSelected())) {
+						throw new NullPointerException();
+					}
+						else {
+							String nome = tfNome.getText();
+							String cpf = ftfCPF.getText();
+							String data = ftfData.getText();
+							String sexo;
+							if(rbF.isSelected()) {
+								sexo = "Feminino";
+							}else {
+								sexo = "Masculino";
+							}
+							
+							Cliente cliente = new Cliente(nome, cpf, data, sexo);
+							controle.adicionarClienteController(cliente);
+							
+							JOptionPane.showMessageDialog(null, "CADASTRADO COM SECESSO", "SUCESSO", JOptionPane.INFORMATION_MESSAGE);
+							
+						}
+				}catch(NullPointerException w) {
+					JOptionPane.showMessageDialog(null, "CAMPO(S) OBRIGATÓRIOS NÃO PREENCHIDOS", "FALHA", JOptionPane.INFORMATION_MESSAGE);
+				} catch(Exception q) {
+					JOptionPane.showMessageDialog(null, "ALGO DEU ERRADO", "FALHA", JOptionPane.INFORMATION_MESSAGE);
+				}
+			}
+		});
+		btnNewButton.setIcon(new ImageIcon(CadastrarCliente.class.getResource("/util/account-multiple-plus.png")));
+		btnNewButton.setBounds(10, 246, 328, 23);
+		contentPane.add(btnNewButton);
+		
+		
+	}
+}
---
+++ src/views/ListarProdutosExcluidos.java
@@ -0,0 +1,104 @@
+package views;
+
+import java.awt.BorderLayout;
+import controller.ControleProduto;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Set;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+import models.Cliente;
+import models.Produto;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+public class ListarProdutosExcluidos extends JFrame {
+
+	private JPanel contentPane;
+	ControleProduto  controle = new ControleProduto();
+	Set<Produto> produtos;
+	private JTable tabela;
+
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ListarProdutosExcluidos frame = new ListarProdutosExcluidos();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ListarProdutosExcluidos() {
+		
+		controle.iniciarProdutos();
+		produtos = controle.listarProdutosExcluidosController();
+		
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 700, 434);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 384, 664, -325);
+		contentPane.add(scrollPane);
+		
+		JLabel lblNewLabel = new JLabel("PRODUTOS EXCLU\u00CDDOS");
+		lblNewLabel.setBounds(10, 11, 664, 28);
+		lblNewLabel.setFont(new Font("Century", Font.PLAIN, 16));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lblNewLabel);
+		
+		tabela = new JTable();
+		tabela.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"NOME", "VALOR", "QUANTIDADE EXCLU\u00CDDA"
+			}
+		) {
+			boolean[] columnEditables = new boolean[] {
+				false, false, false
+			};
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
+			}
+		});
+		tabela.getColumnModel().getColumn(2).setPreferredWidth(133);
+		tabela.setFont(new Font("Century", Font.PLAIN, 12));
+		tabela.setBounds(10, 383, 664, -312);
+		scrollPane.setViewportView(tabela);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+	}
+
+
